2036464edd2967f2226c5f5d6d30161b6368bb3c
ZJQ <wy17674094293@163.com>
2025-10-02 22:04:00 +0800

    fix:完善链路追踪

--- product/handler/product.go
+++ product/handler/product.go
@@ -8,42 +8,43 @@ import (
 	"product/domain/service"
 	. "product/proto/product"
 )
-type Product struct{
-     ProductDataService service.IProductDataService
+
+type Product struct {
+	ProductDataService service.IProductDataService
 }
 
-//添加商品
+// 添加商品
 func (h *Product) AddProduct(ctx context.Context, request *ProductInfo, response *ResponseProduct) error {
 	productAdd := &model.Product{}
 	fmt.Println(request)
-	if err := common.SwapTo(request,productAdd);err!=nil{
+	if err := common.SwapTo(request, productAdd); err != nil {
 		return err
 	}
 	fmt.Println(productAdd)
-	productID,err:=h.ProductDataService.AddProduct(productAdd)
-	if err!=nil{
+	productID, err := h.ProductDataService.AddProduct(productAdd)
+	if err != nil {
 		return err
 	}
 	response.ProductId = productID
 	return nil
 }
 
-//根据ID查找商品
+// 根据ID查找商品
 func (h *Product) FindProductByID(ctx context.Context, request *RequestID, response *ProductInfo) error {
-	productData,err := h.ProductDataService.FindProductByID(request.ProductId)
-	if err !=nil {
+	productData, err := h.ProductDataService.FindProductByID(request.ProductId)
+	if err != nil {
 		return err
 	}
-	if err := common.SwapTo(productData,response);err !=nil {
+	if err := common.SwapTo(productData, response); err != nil {
 		return err
 	}
 	return nil
 }
 
-//商品更新
+// 商品更新
 func (h *Product) UpdateProduct(ctx context.Context, request *ProductInfo, response *Response) error {
 	productAdd := &model.Product{}
-	if err := common.SwapTo(request,productAdd);err!=nil{
+	if err := common.SwapTo(request, productAdd); err != nil {
 		return err
 	}
 	err := h.ProductDataService.UpdateProduct(productAdd)
@@ -53,29 +54,30 @@ func (h *Product) UpdateProduct(ctx context.Context, request *ProductInfo, respo
 	response.Msg = "更新成功"
 	return nil
 }
-//根据ID删除对应商品
+
+// 根据ID删除对应商品
 func (h *Product) DeleteProductByID(ctx context.Context, request *RequestID, response *Response) error {
-	if err := h.ProductDataService.DeleteProduct(request.ProductId);err!=nil {
+	if err := h.ProductDataService.DeleteProduct(request.ProductId); err != nil {
 		return err
 	}
 	response.Msg = "删除成功"
 	return nil
 }
 
-//查找所有商品
-func (h *Product) FindAllProduct(ctx context.Context, request *RequestAll, response *AllProduct) error  {
-	productAll,err := h.ProductDataService.FindAllProduct()
-	if err !=nil {
+// 查找所有商品
+func (h *Product) FindAllProduct(ctx context.Context, request *RequestAll, response *AllProduct) error {
+	productAll, err := h.ProductDataService.FindAllProduct()
+	if err != nil {
 		return err
 	}
 
-	for _,v := range productAll {
+	for _, v := range productAll {
 		productInfo := &ProductInfo{}
-		err := common.SwapTo(v,productInfo)
+		err := common.SwapTo(v, productInfo)
 		if err != nil {
 			return err
 		}
-		response.ProductInfo = append(response.ProductInfo,productInfo)
+		response.ProductInfo = append(response.ProductInfo, productInfo)
 	}
 	return nil
 }
